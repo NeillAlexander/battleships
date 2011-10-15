141efdc7287e398e67e7973b3db86d2fce9ede8b
Neill Alexander <neillalexander@gmail.com>
2011-10-15 23:42:46 +0100

    Added functions to generate the squares that a ship will occupy

--- src/battleships/board.clj
+++ src/battleships/board.clj
@@ -1,5 +1,6 @@
 (ns battleships.board)
 
+;; Data structures
 (defn make-board
   "Create a new board of width w and height h."
   [w h]
@@ -7,6 +8,10 @@
    :height h,
    :squares (apply vector (take (* w h) (repeat 0)))})
 
+(defn make-ship [name key length bit]
+  {:name name, :key key, :length length, :bit bit})
+
+;; Functions that manipulate the data structures
 (defn row
   "return the row for coord, where coord like a3 (would return A)"
   [coord]
@@ -16,7 +21,7 @@
   "return the column part of the coord, where coord like a3"
   [coord]
   ;; columns start at 1 so need to subtract 0 index vector.
-  (dec (Integer/parseInt (apply str (rest coord)))))
+  (Integer/parseInt (apply str (rest coord))))
 
 (defn row-to-int
   "Convert row character (upper case) to an int for indexing."
@@ -36,9 +41,8 @@
 (defmethod transform String
   [{:keys [width height] :as board} coord]
   (let [row-num (row-to-int (row coord))
-        col-num (column coord)]
-    (if-not (or (>= row-num width) (>= col-num height))
-      (+ (* row-num width) col-num))))
+        col-num (dec (column coord))]
+    (+ (* row-num width) col-num)))
 
 ;; transform 0 to A1 etc
 (defmethod transform Integer
@@ -46,3 +50,39 @@
   (let [row-num (quot index width)
         col-num (rem index width)]
     (str (int-to-row row-num) (inc col-num))))
+
+;; functions that generate the indices of the squares when placing a ship
+(defmulti squares
+  "Works out the sequence of squares that will be used for the ship."
+  (fn [coords length orientation] orientation))
+
+;; horizontal orientation
+(defmethod squares :h [coords length _]
+  (let [row (row coords)
+        col (column coords)]
+    (take length (map str (repeat row) (iterate inc col)))))
+
+;; vertical orientation
+(defmethod squares :v [coords length _]
+  (let [row-num (row-to-int (row coords))
+        col (column coords)]
+    (take length (map str (map int-to-row (iterate inc row-num)) (repeat col)))))
+
+(defn between [n l h]
+  (and (>= n l) (<= n h)))
+
+;; function to validate that square is within boundary (needed for placing ships)
+(defn valid-square? [{:keys [width height] :as board} coord]
+  (let [row-num (row-to-int (row coord))
+        col-num (dec (column coord))]
+    (and
+     (between row-num 0 (dec height))
+     (between col-num 0 (dec width)))))
+
+;; functions for placing ship on board
+;; need to be able to query for empty square
+;; need to be able to set a square as occupied
+;; need to be able to tell which ship occupies a square
+(defn empty-square? [board coord])
+
+
--- test/battleships/test/board.clj
+++ test/battleships/test/board.clj
@@ -3,19 +3,34 @@
   (:require [battleships.board] :reload-all))
 
 
-(deftest transform-coords
+(deftest test-transform-coords
   (let [board (make-board 10 10)]
     (is (= 0 (transform board "a1")))
     (is (= 0 (transform board "A1")))
     (is (= 9 (transform board "a10")))
     (is (= 10 (transform board "b1")))
-    (is (= 99 (transform board "j10")))
-    (is (= nil (transform board "a12")))
-    (is (= nil (transform board "k1")))))
+    (is (= 99 (transform board "j10")))))
 
-(deftest transform-index
+(deftest test-transform-index
   (let [board (make-board 10 10)]
     (is (= "A1" (transform board 0)))
     (is (= "A10" (transform board 9)))
     (is (= "B1" (transform board 10)))
     (is (= "J10" (transform board 99)))))
+
+(deftest test-squares
+  (is (= ["A1" "A2" "A3"] (squares "A1" 3 :h)))
+  (is (= ["A1" "A2" "A3" "A4"] (squares "A1" 4 :h)))
+  (is (= ["A1" "B1" "C1"] (squares "A1" 3 :v)))
+  (is (= ["D3" "E3" "F3" "G3"] (squares "D3" 4 :v))))
+
+(deftest test-valid-square
+  (let [board (make-board 10 10)]
+    (is (valid-square? board "A1"))
+    (is (valid-square? board "A10"))
+    (is (valid-square? board "J1"))
+    (is (valid-square? board "J10"))
+    (is (not (valid-square? board "A0")))
+    (is (not (valid-square? board "A11")))
+    (is (not (valid-square? board "J0")))
+    (is (not (valid-square? board "J11")))))
