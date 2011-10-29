5364ef5888bd1b322805306a610528b42b635f2d
Neill Alexander <neillalexander@gmail.com>
2011-10-29 19:05:17 +0100

    Game engine now calls back to Player to place all the ships, with no error handling yet

--- src/battleships/engine.clj
+++ src/battleships/engine.clj
@@ -9,7 +9,7 @@
   "For a game of battleships we need to plug 2 Players into the engine."
   (get-name [this]
     "Should return the name of the player.")
-  (ship-position [this ship-key]
+  (ship-position [this ship]
     "Return the ShipPosition for the ship. This will be called for each ship until all ships are placed on board.")
   (next-shot [this]
     "Return the coordinate for the next shot. Will be called until a valid coord is returned.")
@@ -20,17 +20,35 @@
   (lost [this]
     "Called when this player lost the game."))
 
-(defn- valid-player? [player]
+(defn valid-player? [player]
   (satisfies? Player player))
 
-(defn- ensure-valid-players [player1 player2]
+(defn ensure-valid-players [player1 player2]
   (if-not (and (valid-player? player1)
                (valid-player? player2))
     (throw (IllegalArgumentException. "Invalid players"))))
+
+(defn place-ship
+  [player-key player-impl game ship-key]
+  (let [player-data (game player-key)
+        ships (:ships player-data)
+        ship-pos (ship-position player-impl (ships ship-key))]
+    (assoc game player-key
+           (game/place-ship player-data ship-key (:coord ship-pos) (:orientation ship-pos)))))
+
+(defn place-ships
+  "Calls back to ship-position for every ship until all have been placed successfully."
+  [game player-key player-impl]
+  (reduce (partial place-ship player-key player-impl) game board/ship-keys))
 
 (defn play
   "Call this to start a new game with the 2 players."
   [player1 player2]
   (ensure-valid-players player1 player2)
-  (let [game (game/new-game (get-name player1) (get-name player2))]
+  (let [game (-> (game/new-game (get-name player1) (get-name player2))
+                 (place-ships :player1 player1)
+                 (place-ships :player2 player2))]
     game))
+
+
+;; TODO: place-ship needs to handle invalid / already used coords
--- test/battleships/test/engine.clj
+++ test/battleships/test/engine.clj
@@ -1,11 +1,20 @@
 (ns battleships.test.engine
-  (:use [battleships.engine]
-        [clojure.test]))
+  (:import
+   [battleships.engine ShipPosition])
+  (:use
+   [battleships.engine]
+   [clojure.test]))
+
+(def ship-positions {:aircraft-carrier (ShipPosition. "a1" :h),
+                     :battleship (ShipPosition. "b1" :h),
+                     :destroyer (ShipPosition. "c1" :h)
+                     :submarine (ShipPosition. "d1" :h)
+                     :patrol-boat (ShipPosition. "e1" :h)})
 
 (defn make-test-player [name]
   (reify Player
     (get-name [this] name)
-    (ship-position [this ship-key] nil)
+    (ship-position [this ship] (ship-positions (:key ship)))
     (next-shot [this] nil)
     (shot-result [this coord result])
     (won [this])
