683697213b9f146f18420cefd2059f26b7a37ec6
Neill Alexander <neillalexander@gmail.com>
2011-10-29 21:58:42 +0100

    Now detects the condition where the one or both of the players failed to place ships correctly

--- src/battleships/engine.clj
+++ src/battleships/engine.clj
@@ -9,15 +9,17 @@
   "For a game of battleships we need to plug 2 Players into the engine."
   (get-name [this]
     "Should return the name of the player.")
+  (bot? [this]
+    "Must return true if this is a bot.")
   (ship-position [this ship]
     "Return the ShipPosition for the ship. This will be called for each ship until all ships are placed on board.")
   (next-shot [this]
     "Return the coordinate for the next shot. Will be called until a valid coord is returned.")
   (shot-result [this coord result]
     "Called after the result of the shot is known. Result will be either :hit, :miss, or one of the ship keys if the ship was sunk e.g. :aircraft-carrier.")
-  (won [this]
+  (you-won [this]
     "Called when this player won the game.")
-  (lost [this]
+  (you-lost [this]
     "Called when this player lost the game."))
 
 (defn valid-player? [player]
@@ -29,26 +31,87 @@
     (throw (IllegalArgumentException. "Invalid players"))))
 
 (defn place-ship
+  "Places the ship on the board, repeating until a valid position is provided or (for a bot) the number of attempts exceeds 100"
   [player-key player-impl game ship-key]
-  (let [player-data (game player-key)
-        ships (:ships player-data)
-        ship-pos (ship-position player-impl (ships ship-key))]
-    (assoc game player-key
-           (game/place-ship player-data ship-key (:coord ship-pos) (:orientation ship-pos)))))
+  (loop [num-attempts 0
+         player-data (game player-key)
+         ships (:ships player-data)]
+    ;; if the number of attempts exceeds 100 for a bot, mark it as failed
+    (if (and (> num-attempts 100) (bot? player-impl))
+      (assoc-in game [player-key :failed] true)
+      (let [ship-pos (ship-position player-impl (ships ship-key))
+            updated-player-data (game/place-ship
+                                 player-data ship-key (:coord ship-pos) (:orientation ship-pos))]
+        (if updated-player-data
+          (assoc game player-key updated-player-data)
+          (recur (inc num-attempts) player-data ships))))))
 
 (defn place-ships
   "Calls back to ship-position for every ship until all have been placed successfully."
   [game player-key player-impl]
   (reduce (partial place-ship player-key player-impl) game board/ship-keys))
 
+(defn first-failed-second-ok? [player1 player2]
+  (and (:failed player1) (not (:failed player2))))
+
+(defn the-failed-player [{:keys [player1 player2]}]
+  (cond
+   (first-failed-second-ok? player1 player2) :player1
+   (first-failed-second-ok? player2 player1) :player2
+   :else nil))
+
+(defn count-failures [& players]
+  (reduce (fn [n player] (if (:failed player) (inc n) n)) 0 players))
+
+(defn notify-winning-player [winner-key loser-key player1 player2]
+  (let [winner (if (= :player1 winner-key) player1 player2)
+        loser (if (= :player2 loser-key) player2 player1)]
+    (you-won winner)
+    (you-lost loser)))
+
+(defn init-game [player1 player2]
+  (-> (game/new-game (get-name player1) (get-name player2))
+      (place-ships :player1 player1)
+      (place-ships :player2 player2)))
+
+
+(defn ship-placement-success? [{:keys [player1 player2]}]
+  (= 0 (count-failures player1 player2)))
+
+(defn finished
+  "Checks the end game conditions and notifies accordingly."
+  [{:keys [player1 player2] :as game} player1-impl player2-impl]
+  (let [num-failures (count-failures player1 player2)]
+    (cond
+     (= 1 num-failures) (let [failed-player (the-failed-player game)]
+                          (let [winning-player (if (= :player1 failed-player) :player2 :player1)]
+                            (notify-winning-player winning-player failed-player
+                                                   player1-impl player2-impl)))
+     (= 2 num-failures) (do
+                          (you-lost player1-impl)
+                          (you-lost player2-impl))
+     :else (throw (IllegalStateException. "Unknown finishing condition")))))
+
+(defn run-game-loop
+  "The main loop that asks for moves until someone wins or a bot exceeds the number of allowed errors"
+  [game player1 player2])
+
 (defn play
   "Call this to start a new game with the 2 players."
   [player1 player2]
   (ensure-valid-players player1 player2)
-  (let [game (-> (game/new-game (get-name player1) (get-name player2))
-                 (place-ships :player1 player1)
-                 (place-ships :player2 player2))]
-    game))
+  (let [game (init-game player1 player2)]
+    ;; check to see if either player failed. If one failed and the other didn't then we have a winner
+    (if (ship-placement-success? game)
+      (run-game-loop game player1 player2)
+      (finished game player1 player2))))
 
 
-;; TODO: place-ship needs to handle invalid / already used coords
+
+
+
+
+
+
+
+
--- test/battleships/test/engine.clj
+++ test/battleships/test/engine.clj
@@ -1,9 +1,8 @@
 (ns battleships.test.engine
-  (:import
-   [battleships.engine ShipPosition])
-  (:use
-   [battleships.engine]
-   [clojure.test]))
+  (:use     [battleships.engine]
+            [clojure.test])
+  (:import  [battleships.engine ShipPosition])
+  (:require [battleships.game :as game]))
 
 (def ship-positions {:aircraft-carrier (ShipPosition. "a1" :h),
                      :battleship (ShipPosition. "b1" :h),
@@ -11,14 +10,40 @@
                      :submarine (ShipPosition. "d1" :h)
                      :patrol-boat (ShipPosition. "e1" :h)})
 
-(defn make-test-player [name]
+;; failing because try to place to ships on a1
+(def failing-positions {:aircraft-carrier (ShipPosition. "a1" :h),
+                        :battleship (ShipPosition. "a1" :h),
+                        :destroyer (ShipPosition. "c1" :h)
+                        :submarine (ShipPosition. "d1" :h)
+                        :patrol-boat (ShipPosition. "e1" :h)})
+
+(defn make-test-player [name positions-map result-atom]
   (reify Player
     (get-name [this] name)
-    (ship-position [this ship] (ship-positions (:key ship)))
+    (bot? [this] true)
+    (ship-position [this ship] (positions-map (:key ship)))
     (next-shot [this] nil)
     (shot-result [this coord result])
-    (won [this])
-    (lost [this])))
+    (you-won [this] (reset! result-atom true))
+    (you-lost [this] (reset! result-atom false))))
 
-(def p1 (make-test-player "player 1"))
-(def p2 (make-test-player "player 2"))
+(def p1-result-atom (atom nil))
+(def p1 (make-test-player "player 1" ship-positions p1-result-atom))
+
+(def p2-result-atom (atom nil))
+(def p2 (make-test-player "player 2" ship-positions p2-result-atom))
+
+(def stupid-player-result-atom (atom nil))
+(def stupid-player (make-test-player "failer" failing-positions stupid-player-result-atom))
+
+(deftest test-failing-bot
+  (let [game (game/new-game)]
+    (is (not (:failed (:player2 (place-ships game :player2 p2)))))
+    (is (:failed (:player2 (place-ships game :player2 stupid-player))))))
+
+
+(deftest test-play-with-failing-bot
+  (let [game (game/new-game)]
+    (play p1 stupid-player)
+    (is (true? @p1-result-atom))
+    (is (false? @stupid-player-result-atom))))
