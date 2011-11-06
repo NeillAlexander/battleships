924b4d1d228cf13c47d12227e2e060c7e70a5b1e
Neill Alexander <neillalexander@gmail.com>
2011-11-06 18:45:07 +0000

    Basic matrix of matches is now set up. Need to do the work to play the outstanding matches and update stats

--- src/battleships/core.clj
+++ src/battleships/core.clj
@@ -1,35 +1,29 @@
 (ns battleships.core
   (:require [battleships.engine :as engine]
-            [battleships.board :as board])
+            [battleships.board :as board]
+            [battleships.demo :as demo])
   (:import  [battleships.engine ShipPosition])
   (:gen-class :main true))
 
 
 (defn make-random-cpu-player
   ([name]
-     (let [board (board/make-board)
-           squares (atom (zipmap (range 0 99)
-                                 (map (partial board/transform board) (range 0 99))))
-           orientations [:h :v]]
-       (reify engine/Player
-         (get-name [this] name)
-         (bot? [this] true)
-         (ship-position [this ship]
-           (let [pos (ShipPosition. (get @squares (rand-int (count @squares)))
-                                    (orientations (rand-int (count orientations))))]
-             (println (str name " trying to place " (:name ship) " at " pos))
-             pos))
-         (next-shot [this {:keys [last-shot last-result hits misses ships-sunk]} opponent-context]
-           (println (str name " " last-shot " = " last-result ", ships sunk = " ships-sunk))
-           (let [key (nth (keys @squares) (rand-int (count @squares)))
-                 coord (@squares key)]
-             (swap! squares dissoc key)
-             coord))         
-         (you-won [this {:keys [last-shot last-result hits misses ships-sunk]} opponent-context]
-           (println (str name " " last-shot " = " last-result ", ships sunk = " ships-sunk))
-           (println (str name " won in " (+ (count hits) (count misses)) " shots!")))
-         (you-lost [this player-context opponent-context]
-           (println (str name " lost!")))))))
+     (reify engine/Player
+       (get-name [this] name)
+       (bot? [this] true)
+       (ship-position [this ship]
+         (let [pos (demo/place-ship ship)]
+           (println (str name " trying to place " (:name ship) " at " pos))
+           pos))
+       (next-shot [this {:keys [last-shot last-result hits misses ships-sunk] :as context}
+                   opponent-context]
+         (println (str name " " last-shot " = " last-result ", ships sunk = " ships-sunk))
+         (demo/next-shot context opponent-context))         
+       (you-won [this {:keys [last-shot last-result hits misses ships-sunk]} opponent-context]
+         (println (str name " " last-shot " = " last-result ", ships sunk = " ships-sunk))
+         (println (str name " won in " (+ (count hits) (count misses)) " shots!")))
+       (you-lost [this player-context opponent-context]
+         (println (str name " lost!"))))))
 
 
 (defn test-player [player]
--- src/battleships/server.clj
+++ src/battleships/server.clj
@@ -6,18 +6,58 @@
             [battleships.engine :as engine]
             [compojure.route :as route]
             [compojure.handler :as handler]
-            [ring.util.response :as resp]))
+            [ring.util.response :as resp]
+            [clojure.string :as string]))
 
 
 (def players (atom {}))
+(def match-tracker (atom {}))
+
+(defn make-registered-player [player player-ns]
+  {:name (engine/get-name player)
+   :namespace player-ns
+   :impl player
+   :played 0
+   :won 0
+   :lost 0})
+
+(defn same-players? [player1 player2]
+  (= (:namespace player1) (:namespace player2)))
+
+(defn create-match-up!
+  ([player1 player2]
+     (create-match-up! player1 player2 true))
+  ([player1 player2 match1?]
+     (let [match-id (str (:namespace player1) "-" (:namespace player2))]
+       (swap! match-tracker assoc match-id 0)
+       (if match1? (recur player2 player1 false)))))
+
+(defn update-match-tracker! [new-player]
+  (doseq [player (vals @players)]
+    (if-not (same-players? player new-player)
+      (create-match-up! player new-player))))
+
+(defn play-match! [match limit]
+  (let [[player1-ns player2-ns] (string/split (key match) #"-")
+        num-games (val match)
+        player-map @players
+        player1 (player-map player1-ns)
+        player2 (player-map player2-ns)]
+    (println (str "Ready to play match between " player1-ns " and " player2-ns " " num-games))
+    (let [{:keys [winner loser]} (engine/play (:impl player1) (:impl player2))]
+      (println winner))))
+
+(defn play-all-outstanding-games
+  "Play all the matches up to the limit."
+  [limit]
+  (doseq [match @match-tracker :when (< (val match) limit)]
+    (play-match! match limit)))
 
 (defn register-player!
-  [player player-ns]
-  (swap! players assoc player-ns
-         {:name (engine/get-name player)
-          :played 0
-          :won 0
-          :lost 0}))
+  [player player-ns]  
+  (let [registered-player (make-registered-player player player-ns)]
+    (swap! players assoc (str player-ns) registered-player)
+    (update-match-tracker! registered-player)))
 
 ;; always have the computer in here to play against people
 (register-player! (core/make-random-cpu-player "cpu1") 'battleships.core)
@@ -27,6 +67,7 @@
   (if-let [player-ns (loader/eval-ns s)]
     (let [player (loader/make-player player-ns name)]
       (register-player! player player-ns)
+      (play-all-outstanding-games 10)
       (str player-ns))
     (resp/status "Upload failed" 500)))
 
@@ -39,4 +80,5 @@
 
 (def handler
   (handler/site main-routes))
+
 
