90e3c05f7e2dc730f43bc54cae3edbbf1aeb11fd
Neill Alexander <neillalexander@gmail.com>
2011-11-09 15:26:09 +0000

    Leaderboard is now sorted by the number of wins

--- src/battleships/view.clj
+++ src/battleships/view.clj
@@ -4,10 +4,12 @@
 (defn make-player-table [players]
   (html [:table
          [:tr [:th "Name"] [:th "Played"] [:th "Won"] [:th "Lost"]]
-         (for [{:keys [name played won lost]} (vals players)]
+         (for [{:keys [name played won lost]} (sort (fn [x y] (> (:won x) (:won y))) (vals players))]
            [:tr [:td name] [:td {:align "center"} played]
             [:td {:align "center"} won] [:td {:align "center"} lost]])]))
 
 (defn main-page [players]
   (html [:h1 "Battleships"]
         (make-player-table players)))
+
+
